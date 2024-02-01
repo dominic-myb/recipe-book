c33b27f20b3a81454aa214db82a04d9639f30fbd
dominic-myb <desguerra246@gmail.com>
2024-02-01 22:19:38 +0800

    Added DB

--- app/includes/components/connection.php
+++ app/includes/components/connection.php
@@ -4,6 +4,6 @@ $dbuser = "root";
 $dbpass = "";
 $dbname = "recipe_book_db";
 
-if (!$conn = mysqli_connect($dbhost, $dbuser, $dbpass)) {
+if (!$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)) {
     die("Connection Error" . mysqli_connect_error());
 }
