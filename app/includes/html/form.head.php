c444e26963627f0db147f4bbe1075939e7722706
dominic-myb <desguerra246@gmail.com>
2024-02-02 20:18:05 +0800

    Renamed

---
+++ app/includes/html/form.head.php
@@ -0,0 +1,5 @@
+<head>
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap-5.2.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo $CSS;?>">
+    <title>Recipe Book - <?php echo $title; ?></title>
+</head>
