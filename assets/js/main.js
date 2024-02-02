91bac53ea4c11fc80be9491430ed3e9be6e87f9e
dominic-myb <desguerra246@gmail.com>
2024-02-02 09:52:28 +0800

    form validation using jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,33 @@
+$(document).ready(function () {
+    $("#login-btn").on("click", function (e) {
+        if (!validateForm()) {
+            e.preventDefault();
+        }
+    });
+    $("#register-btn").on("click", function (e){
+        if (!validateForm()) {
+            e.preventDefault();
+        }
+    });
+    function validateForm() {
+        var username = $("#username").val();
+        var pass = $("#password").val();
+        if (username.trim() === "" || pass.trim() === "") {
+            alert("Please fill in all required fields.");
+            return false;
+        }
+        username = DOMPurify.sanitize(username);
+        pass = DOMPurify.sanitize(pass);
 
+        return true;
+    }
+    $("#showPassword").change(function () {
+        var pass = $("#password");
+
+        if ($(this).prop("checked")) {
+            pass.attr("type", "text");
+        } else {
+            pass.attr("type", "password");
+        }
+    });
+});
