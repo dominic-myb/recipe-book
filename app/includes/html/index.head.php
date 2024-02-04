dbe26b516e2e552274a422aebd92dd8da6315647
dominic-myb <desguerra246@gmail.com>
2024-02-04 23:43:50 +0800

    changed title

--- app/includes/html/index.head.php
+++ app/includes/html/index.head.php
@@ -6,5 +6,5 @@
         integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="<?php echo $CSS;?>">
-    <title>Recipe Book - <?php echo $title; ?></title>
+    <title>Culinary Collaborate<?php echo $title; ?></title>
 </head>
