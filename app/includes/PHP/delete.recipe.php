e8a6cb69a599d744d025c879d59621f0d10980a2
dominic-myb <desguerra246@gmail.com>
2024-02-05 02:00:50 +0800

    delete rating

--- app/includes/PHP/delete.recipe.php
+++ app/includes/PHP/delete.recipe.php
@@ -1,21 +1,22 @@
 <?php
 session_start();
-include("app/includes/components/connection.php");
+include("../components/connection.php");
 $userID = $_SESSION['id'];
 $recipeID = $_GET['id'];
 $deleteSQL = "DELETE FROM recipes WHERE user_id = '$userID' AND id = '$recipeID'";
-$result = $conn -> query($deleteSQL);
+$result = $conn->query($deleteSQL);
 $deleteComments = "DELETE FROM comments WHERE recipe_id = '$recipeID'";
-$result = mysqli_query($conn, $deleteComments);
+$result = $conn->query($deleteComments);
+$deleteRatings = "DELETE FROM ratings WHERE recipe_id = '$recipeID'";
+$result = $conn->query($$deleteRatings);
 if ($result) {
     echo "<script>
             alert('Deleted Successfully!')
-            window.location = 'user.recipe.table.php'
+            window.location = '../../../user.recipe.table.php'
         </script>";
 } else {
     echo "<script>
             alert('An Error Occured while Deleting!')
-            window.location = 'user.recipe.table.php'
+            window.location = '../../../user.recipe.table.php'
         </script>";
 }
-?>
