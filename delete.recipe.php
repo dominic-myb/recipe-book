07856c917c84bf697cfd64872cc36b26c8f83ab1
dominic-myb <desguerra246@gmail.com>
2024-02-04 23:20:30 +0800

    Working Delete Recipe

---
+++ delete.recipe.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include("app/includes/components/connection.php");
+$userID = $_SESSION['id'];
+$recipeID = $_GET['id'];
+$deleteSQL = "DELETE FROM recipes WHERE user_id = '$userID'";
+$result = mysqli_query($conn, $deleteSQL);
+$deleteComments = "DELETE FROM comments WHERE recipe_id = '$recipeID'";
+$result = mysqli_query($conn, $deleteComments);
+if ($result) {
+    echo "<script>
+            alert('Deleted Successfully!')
+            window.location = 'user.recipe.table.php'
+        </script>";
+} else {
+    echo "<script>
+            alert('An Error Occured while Deleting!')
+            window.location = 'user.recipe.table.php'
+        </script>";
+}
